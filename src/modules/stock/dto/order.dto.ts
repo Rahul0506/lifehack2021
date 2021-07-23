3478eb0baf0149464d73123cd7eaf55e14bdc18f
Rahul <rahul.gidijala@gmail.com>
2021-07-24 03:51:41 +0800

    Add api documentatin for order body

--- src/modules/stock/dto/order.dto.ts
+++ src/modules/stock/dto/order.dto.ts
@@ -1,5 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class OrderDto {
+    @ApiProperty({
+        required: true,
+    })
     name: string;
+
+    @ApiProperty({
+        required: true,
+    })
     volume: number;
+
+    @ApiProperty({
+        required: true,
+    })
     bidPrice: number;
 }
