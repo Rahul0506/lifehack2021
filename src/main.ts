b8eb3e63926033a9a5d4f6aba0e0306d74e54769
Rahul <rahul.gidijala@gmail.com>
2021-07-23 19:22:01 +0800

    Configure for us

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from "@nestjs/core";
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from "@nestjs/platform-fastify";
-import headers from "fastify-helmet";
-import fastifyRateLimiter from "fastify-rate-limit";
+import * as headers from "helmet";
+import * as rateLimiter from "express-rate-limit";
 import { AppModule } from "./modules/app/app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 /**
  * The url endpoint for open api ui
@@ -31,10 +28,10 @@ export const SWAGGER_API_DESCRIPTION = "API Description";
 export const SWAGGER_API_CURRENT_VERSION = "1.0";
 
 (async () => {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({ logger: true }),
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: console,
+  });
+
   const options = new DocumentBuilder()
     .setTitle(SWAGGER_API_NAME)
     .setDescription(SWAGGER_API_DESCRIPTION)
@@ -43,12 +40,15 @@ export const SWAGGER_API_CURRENT_VERSION = "1.0";
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(SWAGGER_API_ROOT, app, document);
+
   app.enableCors();
-  app.register(headers);
-  app.register(fastifyRateLimiter, {
-    max: 100,
-    timeWindow: 60000,
-  });
+  app.use(headers());
+  app.use(
+    rateLimiter({
+      max: 100,
+      windowMs: 60,
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(9000, "0.0.0.0");
